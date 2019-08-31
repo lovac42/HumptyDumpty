45f8b34a6aac75362edf52c839cec1d79a1bf1ad
lovac42 <lovac42@protonmail.com>
2019-08-31 02:02:20 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# HumptyDumpty
+# Humpty Dumpty
 AnkiAddon: Convert V1-V2; Keep learning cards intact
+
+## About:
+This addon converts between V1 and V2 while keeping learning cards intact. There should be no problems upgrading from V1 to V2, however, downgrading back to V1 from V2 requires some missing values to be recalculated.
+
+If you're not playing around, then you only need to use it once, from V1 to V2 and can safely remove this addon. (Hence the title...)
+
+Please read the source code first before deciding if this is the right addon for you.
+
+## Instruction:
+Preferences > Experimental V2 scheduler
---
+++ src/humpty/dumpty.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Lovac42
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Support: https://github.com/lovac42/HumptyDumpty
+# Version: 0.0.1
+
+
+# Anki maps button2 to good for learning cards on V1.
+# On V2, button2 is mapped to hard.
+
+# If you are playing around switching back and forth
+# between V1 and V2, V2 and V1, then your buttons
+# are merged into one big stack.
+
+# For alternative schedulers addons such as:
+# Plan9, Plan0, or PimsEmu, they are mapped correctly to
+# 4 buttons. So this screws up the stats for alternative
+# schedulers, as it merges hard with good grades.
+
+# If you are using mixed schedulers, like I do,
+# you'll need to separate and export each deck into
+# separate profiles, upgrade each separately, then
+# import these decks back into one.
+
+
+# Turn this off if you don't want your grades merged.
+
+MERGE_AND_REMAP_BUTTONS = True  #Anki's Default Setting
+
+# Setting this to false will also allow you to switch
+# between V1 and V2 without screwing up your stats.
+# But when you are ready to make the switch
+# and you are not using alternative schedulers,
+# make sure to turn this back on.
+
+
+
+
+
+from anki import schedv2
+from aqt import mw
+from anki.utils import intTime
+
+
+def moveToV1(sched):
+    mw.col.clearUndo() #for old versions of anki before 2.1.16
+
+    sched._emptyAllFiltered()
+
+    # Change type 3 to 2, recalculate odue
+    updateAllFromLearning(sched, toSchedVer=2)
+
+    # Convert queue -3 to -2
+    sched._moveManuallyBuried()
+
+    # Convert suspended learning cards to new/review
+    # WARNING: Learning status is lost for suspended cards.
+    sched._resetSuspendedLearning()
+
+    if MERGE_AND_REMAP_BUTTONS:
+        # This migrates btn4 to btn3.
+        # It condenses btn3 with btn2!!
+        # If you also have btn2 data, they are lost.
+        sched._remapLearningAnswers("ease=ease-1 where ease in (3,4)")
+
+
+def moveToV2(sched):
+    mw.col.clearUndo() #for old versions of anki before 2.1.16
+
+    sched._emptyAllFiltered()
+
+    # Change type 2 to 3, remove odue
+    updateAllFromLearning(sched, toSchedVer=1)
+
+    if MERGE_AND_REMAP_BUTTONS:
+        #This migrates btn2 to btn3, and 3 to 4
+        sched._remapLearningAnswers("ease=ease+1 where ease in (2,3)")
+
+
+
+def updateAllFromLearning(sched, toSchedVer=2):
+    """
+        Converts type 2 to type 3
+        V2 sets odue = 0
+        V1 sets odue = due at the time a card lapses
+    """
+
+    if toSchedVer==1: #to V1
+        # LOSSY ODUE:
+        # The real odue values was lost.
+        # We calculate a new number in case
+        # the user bury/suspends the card.
+        # But otherwise, this value is useless.
+        odue=", odue=ivl+%d"%sched.today
+        type=2
+
+    else: #to V2
+        odue=", odue=0"
+        type=3
+
+    sched.col.db.execute("""
+update cards set
+type = %d, mod = %d, usn = %d %s
+where queue in (1,3) and type in (2,3)
+""" % (type, intTime(), sched.col.usn(), odue))
+
+
+
+
+# ========= Wrap ================
+schedv2.Scheduler.moveToV1 = moveToV1
+schedv2.Scheduler.moveToV2 = moveToV2
